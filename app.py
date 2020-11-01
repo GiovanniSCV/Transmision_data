201baf955b9d753766c8da35ee60a02595f76c52
giovanni sabogal cespedes <gsabogal55@ucatolica.edu.co>
2020-10-31 23:26:25 -0500

    Version de servidor funconando de forma concurrente para poder acceder a las urls

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 from flaskext.mysql import MySQL
+import time
+import threading
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb+srv://Prueba:prueba@cluster0.xv7cj.mongodb.net/Prueba?retryWrites=true&w=majority'
@@ -12,50 +14,57 @@ app.config['MYSQL_DATABASE_PASSWORD'] ='9e83483e'
 app.config['MYSQL_DATABASE_DB'] = 'heroku_9b4bacbd64cc96f'
 app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-02.cleardb.com'
 
-
 mysql.init_app(app)
 
+def transpaso_datos():
+    varwhile = True
+    # varwhile = 1
+    while varwhile != False :
+        # user_collection = mongo.db.entries
+        # lista = []
+        # for valFreestyle in user_collection.find():
+        #     valFreestyle.pop('_id')
+        #     valFreestyle['date'] = int(valFreestyle['date'])
+        #     listaAux = valFreestyle.values()
+        #     listas = tuple(listaAux)
+        #     lista.append(listas)
+        # db2 =  mysql.connect()
+        # mycursor = db2.cursor()
+        # querry = "INSERT INTO sensorFreeStyle (date,dateString,rssi,device,direction,rawbg,sgv,type,utcOffset,sysTime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        # error = ""
+        # try:
+        #     mycursor.executemany(querry,lista)
+        #     db2.commit()
+        #     print("Number record inserted, ID:", mycursor.lastrowid)
+        # except:
+        #     print("Eror: "+ error)
+        # db2.close()
+        time.sleep(300)     # similar a delay(segundos)
+        print("Actualizacion de datos")
+    else:
+        print("algo salio malll")
+
+#Usar hilos con threading
+mythreading = threading.Thread(target = transpaso_datos )
+mythreading.setDaemon(True)
+mythreading.start()
 
 @app.route('/')
 def recoger_datos_y_enviar():
-
-    user_collection = mongo.db.entries
-    lista = []
-    for valFreestyle in user_collection.find():
-        valFreestyle.pop('_id')
-        valFreestyle['date'] = int(valFreestyle['date'])
-        listaAux = valFreestyle.values()
-        listas = tuple(listaAux)
-        lista.append(listas)
-
-    for item in lista:
-        print(item)
-    
-    # db2 =  mysql.connect()
-    # mycursor = db2.cursor()
-
-    # querry = "INSERT INTO sensorFreeStyle (date,dateString,rssi,device,direction,rawbg,sgv,type,utcOffset,sysTime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    # error = ""
-    # try:
-    #     mycursor.executemany(querry,lista)
-    #     db2.commit()
-    # except:
-    #     print("Eror: "+ error)
-    # print("Number record inserted, ID:", mycursor.lastrowid)
-    # db2.close() 
+    # for item in lista:
+    #     print(item)
     return render_template('index.html')
 
 @app.route('/bascula',methods = ['POST','GET'])
 def bascula():
     peso = request.form['peso']
     print(peso)
-    response =  request.get_json()
-    print(response)
     db2 =  mysql.connect()
     mycursor = db2.cursor()
     querry = "INSERT INTO pesoUsuario (peso) VALUES (%s)"
     error = ""
     try:
+        print(querry)
         mycursor.executemany(querry,peso)
         db2.commit()
     except:
