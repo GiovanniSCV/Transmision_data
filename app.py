34c9c21ad1a9be436761d6a15a150a1f35b93178
giovanni sabogal cespedes <gsabogal55@ucatolica.edu.co>
2020-12-01 10:08:43 -0500

    Archivo que funciona para eliminar en mongo

--- app.py
+++ app.py
@@ -13,53 +13,78 @@ app.config['MYSQL_DATABASE_USER'] = 'bdb20eaa9eef3c'
 app.config['MYSQL_DATABASE_PASSWORD'] ='d0387f2a'
 app.config['MYSQL_DATABASE_DB'] = 'heroku_42078ca09a517b2'
 app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-02.cleardb.com'
-
 mysql.init_app(app)
 
 def traspaso_datos():
+    user_collection = mongo.db.oldata
+    # vacia = user_collection.find_one({"nombre":"giovanni sabogal cespedes"})
     varwhile = True
-    # varwhile = 1
-    while varwhile != False :
-        # user_collection = mongo.db.entries
-        # lista = []
-        # for valFreestyle in user_collection.find():
-        #     valFreestyle.pop('_id')
-        #     valFreestyle['date'] = int(valFreestyle['date'])
-        #     listaAux = valFreestyle.values()
-        #     listas = tuple(listaAux)
-        #     lista.append(listas)
-        # db2 =  mysql.connect()
-        # mycursor = db2.cursor()
-        # querry = "INSERT INTO sensorFreeStyle (date,dateString,rssi,device,direction,rawbg,sgv,type,utcOffset,sysTime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        # error = ""
-        # try:
-        #     mycursor.executemany(querry,lista)
-        #     db2.commit()
-        #     print("Number record inserted, ID:", mycursor.lastrowid)
-        # except:
-        #     print("Eror: "+ error)
-        # db2.close()
-        time.sleep(300)     # similar a delay(segundos)
+    while varwhile == True :
+        lista = []
+        for valFreestyle in user_collection.find():
+            lista.append(valFreestyle)
+        print(len(lista))
+        for valOldDelete in lista:
+            vaciar = user_collection.find_one({"date":valOldDelete.get("date")})
+            user_collection.find_one_and_delete({"date":valOldDelete.get("date")})
+            print("borrando")
+        print("delay")
+        time.sleep(300)
+    # print("None non tipe")
+    # varwhile = True
+    # try:
+    #     while varwhile != False :
+    #         print("entro")
+    #         user_collection = mongo.db.entries
+    #         lista = []
+    #         oldList = []
+    #         cont = 0
+    #         for valFreestyle in user_collection.find():
+    #             #   Adqisicion datos almacenados
+    #             oldList.append(valFreestyle)
+    #             #   Fin
+    #             valFreestyle.pop('_id')
+    #             valFreestyle['date'] = int(valFreestyle['date'])
+    #             listaAux = valFreestyle.values()
+    #             listas = tuple(listaAux)
+    #             lista.append(listas)
+    #             cont+=1
+    #         print(cont)
+    #         #---------------------------------------------------
+    #         doc = mongo.db.oldata.insert_many(oldList)
+    #         #---------------------------------------------------
+    #         # db2 =  mysql.connect()
+    #         # mycursor = db2.cursor()
+    #         # querry = "INSERT INTO sensorFreeStyle (date,dateString,rssi,device,direction,rawbg,sgv,type,utcOffset,sysTime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    #         # error = ""
+    #         # try:
+    #         #     mycursor.executemany(querry,lista)
+    #         #     db2.commit()
+    #         #     print("Number record inserted, ID:", mycursor.lastrowid)
+    #         # except:
+    #         #     print("Eror: "+ error)
+    #         # db2.close()
+            
+    #         time.sleep(300)     # similar a delay(segundos)
+    #         # db2 =  mysql.connect()
+    #         # mycursor = db2.cursor()
+    #         # querry = "select count(*) from sensorFreeStyle"
+    #         # error = ""
+    #         # try:
+    #         #     mycursor.execute(querry)
+    #         #     countData = mycursor.fetchall()
+    #         #     print(countData)
+    #         #     print("count row :", mycursor.rowcount)
+    #         #     db2.commit()
+    #         # except:
+    #         #     print("Eror: "+ error)
+    #         # db2.close()
+    #         # print(" ")
         
-        db2 =  mysql.connect()
-        mycursor = db2.cursor()
-        querry = "select count(*) from sensorFreeStyle"
-        error = ""
-        try:
-            mycursor.execute(querry)
-            countData = mycursor.fetchall()
-            print(countData)
-            print("count row :", mycursor.rowcount)
-            db2.commit()
-        except:
-            print("Eror: "+ error)
-        db2.close()
-        print(" ")
-        
-        print("Actualizacion de datos")
-    else:
-        print("algo salio malll")
-
+    #         print("Actualizacion de datos")
+    # except:
+    #     print("algo salio malll")
+    
 #Usar hilos con threading
 mythreading = threading.Thread(target = traspaso_datos )
 mythreading.setDaemon(True)
@@ -88,8 +113,7 @@ def home():
     #     print("Eror: "+ error)
     # db2.close()
     # # time.sleep(300)     # similar a delay(segundos)
-    print("Actualizacion de datos")
-
+    # print("Actualizacion de datos")
     return render_template('index.html')
 
 # @app.route('/bascula')
